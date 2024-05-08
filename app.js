75ae8e15c4b7409a77c94162eefa91a44afb7fe6
Mantatas <159446023+Mantatas@users.noreply.github.com>
2024-05-08 13:57:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import {
   getDatabase,
   set,
   ref,
+  get,
+  child,
+  push,
+  update,
+  remove,
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
 const firebaseConfig = {
@@ -17,7 +22,6 @@ const firebaseConfig = {
     "https://mantas-26a39-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
-
 const inputProductCode = document.querySelector("#inputProductCode");
 const inputProductName = document.querySelector("#inputProductName");
 const inputProductQuantity = document.querySelector("#inputProductQty");
@@ -29,20 +33,127 @@ const findButton = document.querySelector("#selectBtn");
 
 const app = initializeApp(firebaseConfig);
 const dataBase = getDatabase(app);
+const dbRef = ref(getDatabase(app));
 
 function insertData(e) {
   e.preventDefault();
-  set(ref(dataBase, "Products/" + inputProductCode.value), {
-    ID: inputProductCode.value,
-    Name: inputProductName.value,
-    Quantity: inputProductQuantity.value,
-  })
-    .then(() => {
-      alert("data added bro");
-    }).catch((error) => {
-      alert(`ya dun goofed. ${error}`);
+  const newProductId = inputProductCode.value;
+
+  get(child(ref(dataBase), "Products/" + newProductId))
+    .then((snapshot) => {
+      if (inputProductCode.value.length === 0) {
+        alert("forgot id idiot");
+        return;
+      }
+      if (inputProductName.value.length === 0) {
+        alert("forgot name idiot");
+        return;
+      }
+      if (inputProductQuantity.value.length === 0) {
+        alert("forgot quantity idiot");
+        return;
+      }
+      if (snapshot.exists()) {
+        alert("Duplicate ID found. Cannot insert data.");
+      } else {
+        set(ref(dataBase, "Products/" + newProductId), {
+          ID: newProductId,
+          Name: inputProductName.value,
+          Quantity: inputProductQuantity.value,
+        })
+          .then(() => {
+            alert("Data added successfully.");
+          })
+          .catch((error) => {
+            alert(`An error occurred: ${error}`);
+          });
+      }
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error checking for duplicate ID:", error);
+      alert("An error occurred while checking for duplicate ID.");
     });
 }
 
+function findData(e) {
+  e.preventDefault();
+  get(child(dbRef, `Products/${findProductCode.value}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const findData = snapshot.val();
+        document.querySelector(
+          "#resultProductCode"
+        ).value = `Product Name: ${findData.Name}`;
+        document.querySelector(
+          "#resultProductQuantity"
+        ).value = `Product Quantity: ${findData.Quantity}`;
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function updateData(e) {
+  e.preventDefault();
+  const newProductId = inputProductCode.value;
+
+  get(child(ref(dataBase), "Products/" + newProductId))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        set(ref(dataBase, "Products/" + newProductId), {
+          ID: newProductId,
+          Name: inputProductName.value,
+          Quantity: inputProductQuantity.value,
+        })
+          .then(() => {
+            alert("Data added successfully.");
+          })
+          .catch((error) => {
+            alert(`An error occurred: ${error}`);
+          });
+      } else {
+        alert("id not found.");
+      }
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error checking for duplicate ID:", error);
+      alert("An error occurred while checking for duplicate ID.");
+    });
+}
+
+deleteButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (productCode.value.length <= 0) {
+    alert("Product code can not be blank!");
+    return;
+  }
+  if (productName.value.length === "") {
+    return;
+  }
+  if (productQuantity.value.length === "") {
+    return;
+  }
+  const productRef = ref(db, "products/" + productCode.value);
+  get(productRef)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        remove(productRef);
+        alert("Deleted successfully!");
+      } else {
+        alert("Product with this code does not exist!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error deleting data:", error);
+    });
+});
+
 insertButton.addEventListener("click", insertData);
-console.log(getDatabase());
+findButton.addEventListener("click", findData);
+updateButton.addEventListener("click", updateData);
+
